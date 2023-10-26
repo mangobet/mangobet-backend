1b32d3a894f1e38e2f00fffa4dd869bd67a318c5
Perico6255 <ruizcanduapedro@gmail.com>
2023-10-26 16:34:27 +0200

    Create test of User class

---
+++ src/test/java/org/ignisus/mangobetbackend/classes/UserTest.java
@@ -0,0 +1,55 @@
+package org.ignisus.mangobetbackend.classes;
+
+    
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.ignisus.enums.UserRole;
+@SpringBootTest
+class UserTest {
+    @Test
+    void testUserConstructors() {
+        User user = new User("1", "test", "test", "test", UserRole.ADMIN);
+        assert(user.getId().equals("1"));
+        assert(user.getUsername().equals("test"));
+        assert(user.getPassword().equals("test"));
+        assert(user.getEmail().equals("test"));
+        assert(user.getRole().equals(UserRole.ADMIN));
+
+        User user2 = new User("test", "test", "test", UserRole.ADMIN);
+        assert(user2.getId() == null); // No ID
+        assert(user2.getUsername().equals("test"));
+        assert(user2.getPassword().equals("test"));
+        assert(user2.getEmail().equals("test"));
+        assert(user2.getRole().equals(UserRole.ADMIN));
+
+        User user3 = new User();    // Empty constructor
+        assert(user3.getId() == null);
+        assert(user3.getUsername() == null);
+        assert(user3.getPassword() == null);
+        assert(user3.getEmail() == null);
+        assert(user3.getRole() == null);
+    }
+    @Test
+    void testUserRoleTypes() {
+        assert(UserRole.ADMIN.toString().equals("ADMIN"));
+        assert(UserRole.NORMAL.toString().equals("NORMAL"));
+        assert(UserRole.OWNER.toString().equals("OWNER"));
+    }
+
+    void testUserSetter(){
+        User user = new User();
+        user.setEmail("test");
+        user.setId("test");
+        user.setPassword("test");
+        user.setRole(UserRole.ADMIN);
+        user.setUsername("test");
+        assert(user.getId().equals("test"));
+        assert(user.getUsername().equals("test"));
+        assert(user.getPassword().equals("test"));
+        assert(user.getEmail().equals("test"));
+        assert(user.getRole().equals(UserRole.ADMIN));
+    }
+
+
+}
